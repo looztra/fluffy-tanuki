ea731d921912dfe2d908ca401c61c413e9dd38d6
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2015-12-06 21:13:00 +0100

    refactor: optimize imports

--- src/main/java/net/devlab722/ft/FluffyTanuki.java
+++ src/main/java/net/devlab722/ft/FluffyTanuki.java
@@ -6,7 +6,6 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class FluffyTanuki {
